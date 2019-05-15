a6071293d94c900f77e2455eadd9c448af187cb5
doradoe1 <doradoe1@outlook.com>
2019-05-14 21:55:13 -0500

    First Commit

---
+++ FrontEnd/verification.js
@@ -0,0 +1,18 @@
+function check(form) {
+    if (form.username.value == "username" && form.password.value == "password" && form.email.value == "email") {
+        window.open('app.html')
+    }
+    else {
+        alert("Error: Incorrect/Missing Username, Password, or Email")
+    }
+};
+
+function check(form) {
+    if (form.username.value == "username" && form.password.value == "password") {
+        window.open('app.html')
+    }
+    else {
+        alert("Error: Incorrect/Missing Username or Password")
+    }
+};
+
