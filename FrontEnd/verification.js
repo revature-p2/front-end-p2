bf699522a01e1c25b3dd41e27a73e670a78f269d
doradoe1 <doradoe1@outlook.com>
2019-05-15 22:16:03 -0500

    Third Commit

--- FrontEnd/verification.js
+++ FrontEnd/verification.js
@@ -1,4 +1,15 @@
-
+if (typeof (Storage) !== "undefined") {
+    // Store the data:!!
+    localStorage.username = "username";
+    localStorage.password = "password";
+    localStorage.email = "email";
+    // Retrieve the data!!
+    document.getElementById("result").innerHTML = localStorage.username;
+    document.getElementById("result").innerHTML = localStorage.password;
+    document.getElementById("result").innerHTML = localStorage.email;
+} else {
+    document.getElementById("result").innerHTML = "No can do baby-doll, your browser does not support Web Storage...";
+};
 
 function check(form) {
     document.getElementById
@@ -18,4 +29,3 @@ function check(form) {
         alert("Error: Incorrect/Missing Username or Password")
     }
 };
-
